de433c904c7fbef41bc23f9671cbb1fc78860f20
ValerioViteritti <vviteritti@gmail.com>
2024-06-19 11:29:12 +0200

    1.0.0 Finito

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
 
 // MILESTONE 1:
-// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
 // MILESTONE 2:
-// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
 // BONUS 1:
 // Trasformare la stringa foto in una immagine effettiva
 // BONUS 2:
@@ -50,3 +50,27 @@ const membriTeam = [
 //   console.log(element.nome, element.ruolo);
 // });
 
+
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+const teamContainer = document.getElementById('team-container');
+
+membriTeam.forEach((element) => {
+  const nomeMembro = `${element.nome}`;
+  const ruoloMembro = `${element.ruolo}`;
+  const fotoMembro = `${element.foto}`;
+  const nomeElement = document.createElement('h2');
+  const ruoloElement = document.createElement('span');
+  const fotoElement = document.createElement('span');
+  nomeElement.textContent = nomeMembro;
+  ruoloElement.textContent = ruoloMembro;
+  fotoElement.textContent = fotoMembro;
+  teamContainer.appendChild(nomeElement);
+  teamContainer.appendChild(ruoloElement);
+  teamContainer.appendChild(fotoElement);
+
+  console.log(nomeMembro);
+  console.log(ruoloMembro);
+  console.log(fotoMembro);
+});
