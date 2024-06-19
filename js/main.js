c91bc5ad26f8c8c6baeeabf180c9dff9df229292
ValerioViteritti <vviteritti@gmail.com>
2024-06-19 10:58:46 +0200

    0.0.2 aggiunti oggetti all'array

---
+++ js/main.js
@@ -0,0 +1,52 @@
+
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
+
+
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+const membriTeam = [
+  {
+    nome: 'Wayne Barrett',
+    ruolo: 'Founder & CEO',
+    foto: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+      nome: 'Angela Carroll',
+      ruolo: 'Chief Editor',
+      foto: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    foto: 'walter-gordon-office-manager.jpg',
+  },
+  {
+      nome: 'Angela Lopez',
+      ruolo: 'Social Media Manager',
+      foto: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    foto: 'scott-estrada-developer.jpg',
+  },
+  {
+      nome: 'Barbara Ramos',
+      ruolo: 'Graphic Designer',
+      foto: 'barbara-ramos-graphic-designer.jpg',
+  },
+];
+
+// membriTeam.forEach((element) => {
+//   console.log(element.nome, element.ruolo);
+// });
+
